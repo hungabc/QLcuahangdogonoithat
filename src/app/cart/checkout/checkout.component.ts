48960ddf4824a1869bbddc91d84f6bc5b32517bb
hungabc <70497683+hungabc@users.noreply.github.com>
2020-10-22 23:15:18 +0700

    22.10.2020

--- src/app/cart/checkout/checkout.component.ts
+++ src/app/cart/checkout/checkout.component.ts
@@ -1,4 +1,6 @@
+import { sha1 } from '@angular/compiler/src/i18n/digest';
 import { Component, Inject, Injector, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { BaseComponent } from 'src/app/lib/base.component';
 
 @Component({
@@ -9,10 +11,49 @@ import { BaseComponent } from 'src/app/lib/base.component';
 export class CheckoutComponent extends BaseComponent implements OnInit {
   giohang:any;
   total:number;
-  tongsl:number
-    constructor(injector:Injector) { super(injector)}
+  tongsl:number;
+  hoadonForm:FormGroup;
+  madonhang:any;
   
+    constructor(injector:Injector) { super(injector)}
+     themdon(value: any) {
+      let  tendangnhap= 'sha1(value.ho_ten)';
+      let khachhang= {TENDANGNHAP:tendangnhap,
+        MATKHAU:null,
+        HOTEN:value.ho+value.ten,
+        GIOITINH:'nam',
+        DIACHI:value.diachi,
+        DIENTHOAI:value.dienthoai,
+        EMAIL:value.email,
+        QUYEN:1,
+        MAKICHHOAT:null,
+        KICHHOAT:1,
+        };
+        this._api.post('/api/Thanhvien/them-KH', khachhang).takeUntil(this.unsubscribe).subscribe(res => {
+           }, err => { });   
+      let donhang = {TENDANGNHAP:tendangnhap,
+          DIACHINGUOINHAN:value.diachi
+        };
+        this._api.post('/api/DonHang/them-DH', donhang).takeUntil(this.unsubscribe).subscribe(res => {
+         this.madonhang=res.MADH;
+           }, err => { });   
+     this.giohang.forEach(chitiet => {
+       let chitietdonhang={MADH:this.madonhang,MASP:chitiet.masp,DONGIA:chitiet.gia ,SOLUONG:chitiet.quantity}
+       this._api.post('/api/CHITIETdh/them-CTDH', chitietdonhang).takeUntil(this.unsubscribe).subscribe(res => {
+         alert('đã thêm đơn hàng');
+         this.clearCart();
+      }, err => { });   
+     });
+   
+    }
     ngOnInit(): void {
+      this.hoadonForm = new FormGroup({
+        ho: new FormControl('', Validators.required),
+        ten: new FormControl('')  , 
+        diachi: new FormControl('') ,  
+        dienthoai: new FormControl('',Validators.min(10))  , 
+        email: new FormControl('',Validators.email)     ,  
+      });
       this._cart.items.subscribe((res) => {
         this.giohang = res;
         this.total = 0;
@@ -25,6 +66,7 @@ export class CheckoutComponent extends BaseComponent implements OnInit {
         
       });
     } 
+ 
     clearCart() { 
       this._cart.clearCart();
       alert('Xóa thành công');
